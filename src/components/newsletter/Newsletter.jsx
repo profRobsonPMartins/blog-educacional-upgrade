3c6daf3ce821b767571a94188c8a272f679156bb
Robson Pereira Martins <profrobson.p.martins2023@gmail.com>
2024-12-02 12:31:25 -0300

    blog commit

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import './Newsletter.css'
 import { useState } from "react";
 
-
-const Newsletter = () => {
+const Contact = () => {
 
     // Lógica aqui
     const [email, setEmail] = useState("")
@@ -35,29 +33,16 @@ const Newsletter = () => {
     }
 
     return (
-        <section className="newsletter-section">
-            <div className="newsletter-container">
-                <h1>EducaTec 3ºB</h1>
-                <h2>Subscribe to our</h2>
-                <h3><span>News</span>Letter</h3>
-                <p>
-                    Fique por dentro das últimas notícias e eventos da nossa escola ao se inscrever
-                    em nossa newsletter. Receba diretamente em seu email informações valiosas
-                    sobre nossos projetos educacionais, novidades, eventos e muito mais!
-                </p>
-                <form className="newsletter-form" onSubmit={handleSubmit}>
-                    <label>Email:</label>
-                    <input type="email"
-                        value={email}
-                        onChange={(e) => (setEmail(e.target.value))}
-                        placeholder="Digite seu email*"
-                    />
-                    <button type="submit">Enviar</button>
-                    {message && <p>{message}</p>}
-                </form>
-            </div>
-        </section>
+        <form onSubmit={handleSubmit}>
+            <label>Email:</label>
+            <input type="email"
+                  value={email}
+                 onChange={(e) => (setEmail(e.target.value))}
+                placeholder="Digite seu email*"
+            />
+            <button type="submit">Enviar</button>
+            {message && <p>{message}</p>}
+        </form>
     )
 }
-
-export default Newsletter;
+export default Contact;
